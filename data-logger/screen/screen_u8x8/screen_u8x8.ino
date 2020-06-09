a9791b630ebdd1de0f8d531f29ca13ffded16e12
gaboflowers <gabriel.flores.m@ug.uchile.cl>
2020-06-09 04:38:50 -0400

    Fixing U8x8 and I2C issue.
    An earlier version of this sketch used the
    U8X8_SSD1306_128X64_NONAME_SW_I2C driver. However, this driver
    conflicted with the Wire.h library, as noted here:
    https://forum.arduino.cc/index.php?topic=482386.0

--- data-logger/screen/screen_u8x8/screen_u8x8.ino
+++ data-logger/screen/screen_u8x8/screen_u8x8.ino
@@ -18,25 +18,23 @@ On Arduino Nano ATMega328P:
 #define CLOCK_PIN A5
 
 // https://github.com/olikraus/u8g2/wiki/u8x8setupcpp#ssd1306-128x64_noname-1
-U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(CLOCK_PIN, DATA_PIN);
+U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE);
 
-/* u8x8.begin() is required and will sent the setup/init sequence to the display */
-void setup(void) {
-  u8x8.begin();
-}
+// u8x8.begin() is required and will sent the setup/init sequence to the display
+void setup(void) { u8x8.begin(); }
 
 uint8_t t = 0;
 
 void loop(void) {
   // Usage: https://github.com/olikraus/u8g2/wiki/u8x8reference
-  // Screen fits only 16x8 chars 
-  
+  // Screen fits only 16x8 chars
+
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.drawString(0,0, "Last measurement");
-  u8x8.setCursor(1,1);
+  u8x8.drawString(0, 0, "Last measurement");
+  u8x8.setCursor(1, 1);
   u8x8.print(t++);
-  u8x8.drawString(0,3, "at");
-  u8x8.drawString(1,4, "2020-02-30");
-  u8x8.drawString(1,5, "12:34:56");
+  u8x8.drawString(0, 3, "at");
+  u8x8.drawString(1, 4, "2020-02-30");
+  u8x8.drawString(1, 5, "12:34:56");
   delay(1000);
 }
