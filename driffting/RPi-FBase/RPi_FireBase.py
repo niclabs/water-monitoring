3629691b5d52b40278fa6951dc4f785fc5614d85
Maximiliano Jones <maximilianojones@gmail.com>
2021-02-01 22:08:36 -0500

    Adding python notebook to download the data to xls

--- driffting/RPi-FBase/RPi_FireBase.py
+++ driffting/RPi-FBase/RPi_FireBase.py
@@ -1,10 +1,14 @@
+# %%
 """
 Tutorial: Send Data to Firebase Using Raspberry Pi
 Hardware:
 – Raspberry Pi 3 Model B
-– ADS1x15
+– ADS1x15 ADC with analogic sensors
+- DS18b20 sensors
+
 References:
 – https://circuitpython.readthedocs.io/projects/mlx90614/en/latest/
+- https://tutorial.cytron.io/2020/12/09/send-data-to-firebase-using-raspberry-pi/
 – https://github.com/thisbejim/Pyrebase
 """
  
@@ -45,7 +49,8 @@ adc = Adafruit_ADS1x15.ADS1015()
 # See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
 GAIN = 1
 
-#%%%%%% dejar tutorial
+#%% Configuración de la Base de datos:
+
 config = {
  "apiKey": "Q1Efd77KWUK60suxePFpIPo9tZvoqidnI2rl4SmL",
  "authDomain": "water-monitoring-46b05.firebaseapp.com",
@@ -53,25 +58,30 @@ config = {
  "storageBucket": "project-id.appspot.com"
 }
  
+# Se inicializa la conexión a firebase
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
  
-print('Reading ADS1x15 values, press Ctrl-C to quit...') 
-print("And send Data to Firebase Using Raspberry Pi")
-print("—————————————-")
-print()
- 
+print('Reading ADS1x15 values, DS18B20 temperature values,') 
+print(" Data to Firebase Using Raspberry Pi")
+print('press Ctrl-C to quit...')
 
+ 
+cabeceras = ['timestamp', 'ch_1', 'ch_2','ch_3','ch_4','temp_1','temp_2','temp_3']
 # Print nice channel column headers.
-print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} | {4:>18} |'.format(*range(5)))
-print('-' * 60)
+print('| {0:>18} | {1:>6} | {2:>6} | {3:>6} | {4:>6} | {5:>6} | {6:>6} | {7:>6} |'.format(*cabeceras))
+print('-' * 87)
+
+
+#%% Loop:
 # Main loop.
 while True:
+    t0 = time.time()
     # Read all the ADC channel values in a list.
     values = [0]*5
     for i in range(4):
         # Read the specified ADC channel using the previously set gain value.
-        values[i] = adc.read_adc(i, gain=GAIN)
+        values[i+1] = adc.read_adc(i, gain=GAIN)
         # Note you can also pass in an optional data_rate parameter that controls
         # the ADC conversion time (in samples/second). Each chip has a different
         # set of allowed data rate values, see datasheet Table 9 config register
@@ -80,29 +90,16 @@ while True:
         # Each value will be a 12 or 16 bit signed integer value depending on the
         # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
     
-    values[4] = time.time()
-    raw1str = "{:.2f}".format(values[0])
-    raw2str = "{:.2f}".format(values[1])
-    raw3str = "{:.2f}".format(values[2])
-    raw4str = "{:.2f}".format(values[3])
+    values[0] = t0
+    raw1str = "{:.2f}".format(values[1])
+    raw2str = "{:.2f}".format(values[2])
+    raw3str = "{:.2f}".format(values[3])
+    raw4str = "{:.2f}".format(values[4])
     
     raw1 = float(raw1str)
     raw2 = float(raw2str)
     raw3 = float(raw3str)
-    raw4 = float(raw4str)
-    
-    # print("log 1: {} ".format(raw1str))
-    
-    # print("log 2: {} ".format(raw2str))
-    # print("log 3: {} ".format(raw3str))
-    # print("log 4: {} ".format(raw4str))
-    # print()
-    
-    
-    time.sleep(2)
-    # Print the ADC values.
-    print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} | {4:>12} |'.format(*values))
-    # Pause for half a second.
+    raw4 = float(raw4str)    
 
     
     Directorios = glob.glob( "/sys/bus/w1/devices/28*/"+"*temperature")
@@ -123,7 +120,7 @@ while True:
         n_sensores = len(Directorios) 
     
     data = {
-    "timestamp": values[4],
+    "timestamp": values[0],
     "channel 1": raw1,
     "channel 2": raw2,
     "channel 3": raw3,
@@ -132,8 +129,16 @@ while True:
     "ds18b20_2": temperatura[1],
     "ds18b20_3": temperatura[2],
     }
-    print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} | {4:>12} |'.format(*values))
-    print('{0:>6} | {1:>6} | {2:>6} |'.format(*temperatura))
+    values+=temperatura
+    print('| {0:>18} | {1:>6} | {2:>6} | {3:>6} | {4:>6} | {5:>6} | {6:>6} | {7:>6} |'.format(*values))
+    tf=time.time()
+
+    while tf-t0 < 30:
+        time.sleep(0.01)
+        tf=time.time()
+    # print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} | {4:>12} |'.format(*values))
+    # print('{0:>6} | {1:>6} | {2:>6} |'.format(*temperatura))
     db.child("datalogger").child("1-set").set(data)
     db.child("datalogger").child("2-push").push(data)   
-    time.sleep(1)
+
+ 
--- driffting/RPi-FBase/json_to_xls.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df_json = pd.read_json('/home/pi/Documents/water-monitoring-46b05-default-rtdb-2-push-export.json', lines=True)
-# df_json.to_excel('DATAFILE_PUSH.xlsx')
-
-#%%
-df_json.head()
