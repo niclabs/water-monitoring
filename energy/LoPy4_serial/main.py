b43d4d3338afa04193bc8399c3ac7a5b4fe9428a
Sebastian Cifuentes <cifuentes.sa@gmail.com>
2021-05-09 23:36:18 -0400

    Add LoPy4 code used in energy measurement

--- energy/LoPy4_serial/main.py
+++ energy/LoPy4_serial/main.py
@@ -5,10 +5,7 @@ def get_lorawan_maximum_payload_size(dr):
     frm_payload_size = mac_payload_size_dic.get(str(dr)) - fhdr_size - fport_size
     return frm_payload_size
 
-def checksum_validator(buff):
-    return True
-
-def remove_checksum_bytes(buff):
+def get_data_pointers(buff):
     block_size = 51 # include 2 bytes for Checksum
     n_block = math.ceil(len(buff)/block_size)
 
@@ -21,7 +18,7 @@ def remove_checksum_bytes(buff):
         else:
             ptr_end = block_size*(i+1)-2
 
-        if checksum_validator(buff[ptr_start:ptr_end]):
+        if check_fletcher16(buff[ptr_start:ptr_end+2]):
             print("LoPy4 - PROCESS - Checksum OK")
             ptrs.append((ptr_start,ptr_end))
         else:
@@ -32,18 +29,9 @@ def remove_checksum_bytes(buff):
 
 
 
-
-
-
 print("LoPy4 - Initializing communication values...")
-print("LoPy4 - SERIAL_RX - Cleaning Serial Rx buffer")
-n_bytes = uart.any()
-buff = uart.read(n_bytes)
-print("LoPy4 - SERIAL_RX - num bytes recv and dropped: %d" % n_bytes)
 
-#s.send(bytes([0xff]))
-#m_stats = lora.stats()
-#m_dr = m_stats[4]
+
 m_dr = DATA_RATE
 print("LoPy4 - LoRaWAN - Initial Data Rate: ", m_dr)
 lorawan_mtu = get_lorawan_maximum_payload_size(m_dr)
@@ -55,11 +43,14 @@ a = 1
 
 while True:
     print("*****************************")
+    print("LoPy4 - SERIAL_RX - Cleaning Serial Rx buffer")
+    n_bytes = uart.any()
+    buff = uart.read(n_bytes)
+    print("LoPy4 - SERIAL_RX - num bytes recv and dropped: %d" % n_bytes)
     print("LoPy4 - Loop %d" % a)
     a=a+1
     print("LoPy4 - Going to sleep...")
-    #time.sleep(60)
-    machine.sleep(100000)
+    machine.sleep(9000000)
 
     print("Weak up...")
 
@@ -83,33 +74,45 @@ while True:
         print("LoPy4 - LoRaWAN - Maximum Payload Size: %d bytes" % lorawan_mtu)
 
 
-        ptrs = remove_checksum_bytes(recv_bytes)
+        ptrs = get_data_pointers(recv_bytes)
 
-        measure_size = 7    # bytes
-        block_size = 51     # bytes
-        n_availables_measures = lorawan_mtu//measure_size
-        n_measures = (len(recv_bytes) - math.ceil(len(recv_bytes)/block_size)*2)/measure_size
+        block_size = 49     # bytes
+        blocks_per_mtu = lorawan_mtu//block_size    # n° blocks per a LoRaWAN message
+        n_lorawan_messages = math.ceil(len(ptrs)/blocks_per_mtu)
+        n_blocks_in_full_lorawan_msg = blocks_per_mtu * (len(ptrs)//blocks_per_mtu)
+        print("LoPy4 - LoRaWAN - The current LoRaWAN MTU supports %d blocks" % blocks_per_mtu)
+        print("LoPy4 - LoRaWAN - The %d blocks are sent in multiple (%d) LoRaWAN messages" % (len(ptrs),n_lorawan_messages))
 
-        print("LoPy4 - LoRaWAN - The current LoRaWAN MTU supports %d measures" % n_availables_measures)
-        print("LoPy4 - LoRaWAN - The current serial message has %d measures of 7 bytes each" % n_measures)
-        if(n_measures <= n_availables_measures):
-            # el tamaño de la MTU permite enviar todos los payloads de una vez
-            print("LoPy4 - LoRaWAN - The %d measures are sent in only a LoRaWAN message" % n_measures)
-            aux = bytes()
-            for j in ptrs:
-                aux = aux + recv_bytes[j[0]:j[1]]
-            s.send(bytes(aux))
-        else:
-            # el tamaño de la MTU NO permite enviar todos los payloads de una vez.
-            # Se deben enviar varios mensajes LoRaWAN
-            n_lorawan_messages = math.ceil(n_measures/n_availables_measures)
-            print("LoPy4 - LoRaWAN - The %d measures are sent in multiple (%d) LoRaWAN messages" % (n_measures,n_lorawan_messages))
-            aux = bytes()
-            for k in ptrs:
-                aux = aux + recv_bytes[k[0]:k[1]]
-            for x in range(1,n_lorawan_messages+1):
-                pass
-                #s.send(bytes(aux[(x-1)*n_availables_measures*measure_size:x*n_availables_measures*measure_size]))
 
-        m_stats = lora.stats()
-        m_dr = m_stats[4]
+        print("LoPy4 - LoRaWAN - blocks_per_mtu: %d" % blocks_per_mtu)
+        print("LoPy4 - LoRaWAN - len(ptrs): %d" % len(ptrs))
+        print("LoPy4 - LoRaWAN - n_blocks_in_full_lorawan_msg: %d" % n_blocks_in_full_lorawan_msg)
+
+        i = 0
+        while i < n_blocks_in_full_lorawan_msg:
+            aux = bytearray()
+            for _ in range(blocks_per_mtu):
+                aux += recv_bytes[ptrs[i][0]:ptrs[i][1]]
+                i += 1
+            try:
+                s.setblocking(True)
+                print("LoPy4 - LoRaWAN - Sending %d bytes" % len(aux))
+                s.send(aux)
+                s.setblocking(False)
+            except OSError as e:
+                if e.args[0] == 11:
+                    s.setblocking(False)
+                    print("LoPy4 - LoRaWAN_ERROR - It can probably be a duty cycle problem")
+        if n_blocks_in_full_lorawan_msg != len(ptrs):
+            aux = bytearray()
+            for i in range(n_blocks_in_full_lorawan_msg, len(ptrs)):
+                aux += recv_bytes[ptrs[i][0]:ptrs[i][1]]
+            try:
+                s.setblocking(True)
+                print("LoPy4 - LoRaWAN - Sending %d bytes" % len(aux))
+                s.send(aux)
+                s.setblocking(False)
+            except OSError as e:
+                if e.args[0] == 11:
+                    s.setblocking(False)
+                    print("LoPy4 - LoRaWAN_ERROR - It can probably be a duty cycle problem")
