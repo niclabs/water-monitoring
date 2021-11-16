9b34cc86c0840b27abf4156d168521f193635f00
matimacayal <keny92@gmail.com>
2021-11-16 13:01:19 -0300

    Sketchs para experimentos iteración 4

---
+++ drifting/Iteración 4/FONDEF_drifting_arduino_datalogger_it4_serial2nodemcu/FONDEF_drifting_arduino_datalogger_it4_serial2nodemcu.ino
@@ -0,0 +1,248 @@
+#include <Wire.h>
+#include <Arduino.h>
+#include "SdFat.h" // Bill Greiman
+#include <avr/sleep.h>
+#include <SoftwareSerial.h>
+#include "RTClib.h"
+#include <Adafruit_ADS1X15.h>
+#include <SPI.h>
+#include "SdFat.h"
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define VERBOSE_PRINT false
+float volts0, volts1, volts2, volts3;
+float t_atm, p_atm, h_atm;
+
+// ---------------------- BME280 -----------------------
+Adafruit_BME280 bme; // I2C
+
+// ---------------------- ADC --------------------------
+Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
+
+// ---------------------- RTC --------------------------
+#define CLOCK_INTERRUPT_PIN 2
+#define SENSE_FLAG 0x01
+#define SEND_FLAG 0x02
+#define SENSING_FREQ_SECS 5
+RTC_DS3231 rtc;
+uint32_t alarm_time;
+volatile uint32_t elapsed_time;
+volatile uint8_t alarm_flag;
+
+// ----------------------- SD --------------------------
+const uint8_t chipSelect = 10;  // SD chip select pin.  Be sure to disable any other SPI devices such as Enet.
+#define FILE_BASE_NAME "Data"   // Log file base name.  Must be six characters or less.
+SdFat sd;                       // File system object.
+SdFile file;                    // Log file.
+
+#define error(msg) sd.errorHalt(F(msg))
+
+void writeHeader() {
+  file.print(F("unixtime"));
+  file.print(F(",timestamp"));
+  file.print(F(",A0 - Presión"));
+  file.print(F(",A1 - TDS1"));
+  file.print(F(",A2 - TDS2"));
+  file.print(F(",A3 - TDS3"));
+  file.print(F(",T atm"));
+  file.print(F(",P atm"));
+  file.print(F(",H atm"));
+  file.println();
+}
+
+void logData() {
+
+  DateTime now = rtc.now();
+  
+  file.print(now.unixtime(), DEC);
+  
+  file.print(',');
+  file.print(now.year(), DEC);    file.print('/');
+  file.print(now.month(), DEC);   file.print('/');
+  file.print(now.day(), DEC);     file.print(" ");
+  file.print(now.hour(), DEC);    file.print(':');
+  file.print(now.minute(), DEC);  file.print(':');
+  file.print(now.second(), DEC);
+
+  // Write sensor data to CSV record.
+  file.write(',');file.print(volts0);
+  file.write(',');file.print(volts1);
+  file.write(',');file.print(volts2);
+  file.write(',');file.print(volts3);
+  file.write(',');file.print(volts3);
+  file.write(',');file.print(volts3);
+  file.write(',');file.print(volts3);
+
+  file.println();
+}
+
+void printData() {
+  DateTime now = rtc.now();
+  int unixTime = now.unixtime();
+
+  String httpRequestData =  String("{") +
+                                  "\"app_id\": \"app123\"," +
+                                  "\"dev_id\": \"rasp01\"," +
+                                  "\"payload_raw\": \"[" +
+                                      "{'i': 201, 'v':" + String(volts1) + ", 't': " + String(unixTime) + "000000000}," +
+                                      "{'i': 202, 'v':" + String(volts2) + ", 't': " + String(unixTime) + "000000000}," +
+                                      "{'i': 203, 'v':" + String(volts3) + ", 't': " + String(unixTime) + "000000000}" +
+                                    "]\"" +
+                                String("}");
+  Serial.println(httpRequestData);
+  delay(50);
+}
+
+void waking_up() {
+  // Trigered with rtc wake up.
+}
+
+void sleep() {
+  detachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN)); //De dónde recibirá la interrupción para despertar
+  sleep_enable();
+  uint32_t dist;
+  
+  attachInterrupt(digitalPinToInterrupt(CLOCK_INTERRUPT_PIN), waking_up, FALLING);
+  rtc.clearAlarm(1);
+  alarm_time = rtc.now().unixtime() + SENSING_FREQ_SECS;
+  rtc.setAlarm1(DateTime(alarm_time), DS3231_A1_Second);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_cpu();
+}
+
+void setup() {
+  Serial.begin(9600);
+  
+  // ---------------------- ADC ----------------------
+  ads.begin();
+  ads.setGain(GAIN_ONE);
+
+  // ---------------------- RTC ----------------------
+  if (!rtc.begin()) {
+      Serial.println("Couldn't find RTC");
+      while (1);
+  }
+
+  if (rtc.lostPower()) {
+      Serial.println("RTC lost power, lets set the time!");
+      rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  // ----------------------- RTC Alarms ---------------
+  rtc.disable32K();
+  pinMode(CLOCK_INTERRUPT_PIN, INPUT_PULLUP);
+  rtc.writeSqwPinMode(DS3231_OFF);
+  rtc.disableAlarm(1);
+  rtc.disableAlarm(2);
+  rtc.clearAlarm(1);
+  rtc.clearAlarm(2);
+
+  // ----------------------BME280 ---------------------
+  unsigned status;
+  status = bme.begin(0x76);
+  if (!status) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+      while (1) delay(10);
+  }
+
+  // -------------------- SD CARD ---------------------
+  const uint8_t BASE_NAME_SIZE = sizeof(FILE_BASE_NAME) - 1;
+  char fileName[13] = FILE_BASE_NAME "00.csv";
+  
+  // Initialize at the highest speed supported by the board that is
+  // not over 50 MHz. Try a lower speed if SPI errors occur.
+  if (!sd.begin(chipSelect, SD_SCK_MHZ(50))) {
+    sd.initErrorHalt();
+  }
+
+  // Find an unused file name.
+  if (BASE_NAME_SIZE > 6) {
+    error("FILE_BASE_NAME too long");
+  }
+  while (sd.exists(fileName)) {
+    if (fileName[BASE_NAME_SIZE + 1] != '9') {
+      fileName[BASE_NAME_SIZE + 1]++;
+    } else if (fileName[BASE_NAME_SIZE] != '9') {
+      fileName[BASE_NAME_SIZE + 1] = '0';
+      fileName[BASE_NAME_SIZE]++;
+    } else {
+      error("Can't create file name");
+    }
+  }
+  if (!file.open(fileName, O_WRONLY | O_CREAT | O_EXCL)) {
+    error("file.open");
+  }
+  // Read any Serial data.
+  do {
+    delay(10);
+  } while (Serial.available() && Serial.read() >= 0);
+
+  if (VERBOSE_PRINT) {
+    Serial.print(F("Logging to: "));
+    Serial.println(fileName);
+    Serial.println(F("Type any character to stop"));
+  }
+
+  // Write data header.
+  writeHeader();
+
+  delay(10000);
+}
+  
+void loop() {
+  //volts0 = ads.computeVolts(ads.readADC_SingleEnded(0));
+  //volts1 = ads.computeVolts(ads.readADC_SingleEnded(1));
+  //volts2 = ads.computeVolts(ads.readADC_SingleEnded(2));
+  //volts3 = ads.computeVolts(ads.readADC_SingleEnded(3));
+
+  volts0 = ads.readADC_SingleEnded(0);
+  volts1 = ads.readADC_SingleEnded(1);
+  volts2 = ads.readADC_SingleEnded(2);
+  volts3 = ads.readADC_SingleEnded(3);
+  t_atm = bme.readTemperature();
+  p_atm = bme.readPressure() / 100.0F;
+  h_atm = bme.readHumidity();
+
+  DateTime now = rtc.now();
+  file.print(now.unixtime(), DEC);
+  file.print(',');
+  file.print(now.year(), DEC);  file.print('/');
+  file.print(now.month(), DEC); file.print('/');
+  file.print(now.day(), DEC);   file.print(" ");
+  file.print(now.hour(), DEC);  file.print(':');
+  file.print(now.minute(), DEC);file.print(':');
+  file.print(now.second(), DEC);
+  file.write(',');file.print(volts0);
+  file.write(',');file.print(volts1);
+  file.write(',');file.print(volts2);
+  file.write(',');file.print(volts3);
+  file.write(',');file.print(t_atm);
+  file.write(',');file.print(p_atm);
+  file.write(',');file.print(h_atm);
+  file.println();
+  
+  long unixTime = now.unixtime();
+  Serial.print("{");
+  Serial.print("\"app_id\": \"app123\",");
+  Serial.print("\"dev_id\": \"rasp01\",");
+  Serial.print("\"payload_raw\": \"[");
+  Serial.print("{'i': 206, 'v':" + String(volts0) + ", 't': "); Serial.print(unixTime); Serial.print("000000000},");
+  Serial.print("{'i': 201, 'v':" + String(volts1) + ", 't': "); Serial.print(unixTime); Serial.print("000000000},");
+  Serial.print("{'i': 202, 'v':" + String(volts2) + ", 't': "); Serial.print(unixTime); Serial.print("000000000},");
+  Serial.print("{'i': 203, 'v':" + String(volts3) + ", 't': "); Serial.print(unixTime); Serial.print("000000000},");
+  Serial.print("{'i': 207, 'v':" + String(t_atm) + ", 't': "); Serial.print(unixTime); Serial.print("000000000},");
+  Serial.print("{'i': 208, 'v':" + String(p_atm) + ", 't': "); Serial.print(unixTime); Serial.print("000000000},");
+  Serial.print("{'i': 209, 'v':" + String(h_atm) + ", 't': "); Serial.print(unixTime); Serial.print("000000000}");
+  Serial.print("]\"");
+  Serial.print("}");
+  Serial.print("\n");
+  delay(50);
+  
+  // Force data to SD and update the directory entry to avoid data loss.
+  if (!file.sync() || file.getWriteError()) {
+    error("write error");
+  }
+  
+  delay(10);
+  sleep();
+}
---
+++ drifting/Iteración 4/FONDEF_drifting_nodemcu_HTTP_POST_niclabs/FONDEF_drifting_nodemcu_HTTP_POST_niclabs.ino
@@ -0,0 +1,95 @@
+/*
+  Rui Santos
+  Complete project details at Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-http-get-post-arduino/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  Code compatible with ESP8266 Boards Version 3.0.0 or above 
+  (see in Tools > Boards > Boards Manager > ESP8266)
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+//-----------------------------------------------------------
+//------------------- CONFIGURAR WIFI -----------------------
+const char* ssid = "VTR-2968799 2.4G";
+const char* password = "cs6bhYnspgdp";
+//-----------------------------------------------------------
+//-----------------------------------------------------------
+
+const char* serverName = "http://agua.niclabs.cl/data/http";
+const char* queryApiKey = "919c5e5e086a492398141c1ebd95b711";
+
+int unixTime = 1636749718;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 5000;
+
+void setup() {
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  //Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+  Serial.print("Waiting for payload.. ");
+}
+
+void loop() {
+  //if ((millis() - lastTime) > timerDelay) {
+  if (Serial.available() > 0) {
+    //Check WiFi connection status
+    Serial.print("Serial stream received.. ");
+    if(WiFi.status()== WL_CONNECTED){
+      Serial.print("Wifi connected.. ");
+      WiFiClient client;
+      HTTPClient http;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverName);
+      http.addHeader("Content-Type", "application/json");
+      http.addHeader("query-api-key", queryApiKey);
+      //String httpRequestData =  String("{") +
+      //                            "\"app_id\": \"app123\"," +
+      //                            "\"dev_id\": \"rasp01\"," +
+      //                            "\"payload_raw\": \"[" +
+      //                                "{'i': 201, 'v':202.0, 't': " + String(unixTime) + "000000000}," +
+      //                                "{'i': 202, 'v':74.0, 't': " + String(unixTime) + "000000000}," +
+      //                                "{'i': 203, 'v':-1.0,  't': " + String(unixTime) + "000000000}" +
+      //                              "]\"" +
+      //                          String("}");
+
+      String httpRequestData = Serial.readStringUntil('\n');
+      Serial.println(httpRequestData);
+      Serial.println("");
+      int httpResponseCode = http.POST(httpRequestData);
+     
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+        
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+    unixTime += 5;
+
+    Serial.print("Waiting for payload.. ");
+  }
+}
+
+// 19:18:27.333 -> 
+// {"app_id": "app123","dev_id": "rasp01","payload_raw": "[{'i': 201, 'v':3.00, 't': 1636744629000000000},{'i': 202, 'v':2.00, 't': 1636744629000000000},{'i': 203, 'v':2.00, 't': 1636744629000000000}]"}
+// {"app_id": "app123","dev_id": "rasp01","payload_raw": "[{'i': 201, 'v':3.00, 't': 1636744629000000000}]"}
